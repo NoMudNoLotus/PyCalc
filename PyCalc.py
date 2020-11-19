96cb142f14ccc6c36caad8c73be36c4a9ff603af
Nate <52894316+NoMudNoLotus@users.noreply.github.com>
2020-11-19 03:03:04 -0500

    Update PyCalc.py

--- PyCalc.py
+++ PyCalc.py
@@ -1,32 +1,75 @@
-#!/usr/bin/python3
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                         Made By: NoMudNoLotus                               #
-#  A basic calculator for multiplication, Divison, Addition, and Subtraction  #
-#  My first program in the Numbers SubCategory of karan's "MEGA PROJECT LIST" #
-#  GitHub link to the "Mega Project List": https://kutt.it/SUJ7Os             #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#!/usr/bin/env Python
+# completely functional wxpython calculator
 
-def Calc(a, b, opt):
-    # Cats, formats, and prints the params
-    if (opt == "+"):
-        print( str(a) + " " + str(opt) + " " + str(b) + " = " + str(int(a) + int(b)) )
-    if (opt == "-"):
-        print( str(a) + " " + str(opt) + " " + str(b) + " = " + str(int(a) - int(b)) )
-    if (opt == "*"):
-        print( str(a) + " " + str(opt) + " " + str(b) + " = " + str(int(a) * int(b)) )
-    if (opt == "+"):
-        print( str(a) + " " + str(opt) + " " + str(b) + " = " + str(int(a) / int(b)) )
+import wx
 
-def main():  # Wrapper function
-    opt = input("What kind of operation would you like to do?\nChoose between + - * / : ")
-    if (opt not in "+-*/"):
-        opt = input("Please choose a character for an arithmetic operation; must choose between '+' '-' '*' '/' : ")
-    a = input("Please input value 1: ")
-    b = input("Please input value 2: ")
 
-    print(Calc(a, b, opt))
-    
+class Interface(wx.Frame):
+    def __init__(self, parent, title):
+        super(Interface, self).__init__(parent, title=title, size=(300, 400))
+
+        # show window on screen
+        self.setup()
+        self.Show()
+
+    def setup(self):
+        box = wx.BoxSizer(wx.VERTICAL)
+        grid = wx.GridSizer(5, 4, 10, 10)
+
+        # add textbox for output
+        self.textbox = wx.TextCtrl(self, style=wx.TE_RIGHT)
+        box.Add(self.textbox, flag=wx.EXPAND | wx.TOP | wx.BOTTOM, border=4)
+
+        buttons = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            '0', '.', 'C', '+',
+            '='
+        ]
+
+        # create buttons
+        for label in buttons:
+            button = wx.Button(self, -1, label)
+            grid.Add(button, 0, wx.EXPAND)
+            self.Bind(wx.EVT_BUTTON, self.on_button_press, button)
+
+        box.Add(grid, proportion=1, flag=wx.EXPAND)
+        self.SetSizer(box)
+
+    def on_button_press(self, e):
+
+        label = e.GetEventObject().GetLabel()
+
+        calculation = self.textbox.GetValue()
+
+        # handle event based on what button is pressed
+        if label == '=':
+            if not calculation:
+                return
+
+            try:
+                result = eval(calculation)
+            except SyntaxError as err:
+                wx.LOG_Error('Invalid syntax ({}). Please check your input and try again.'.format(calculation))
+                return
+            except NameError as err:
+                wx.LOG_Error('There was an error. Please check your input and try again.')
+                return
+
+            # show result
+            self.textbox.SetValue(str(result))
+
+        elif label == 'C':  # clear if C
+            self.textbox.SetValue('')
+        else:  # 0-9
+            self.textbox.SetValue(calculation + label)  # add the label of btn to calculation
+
 
 if __name__ == '__main__':
-    main()
+    # create application object
+    app = wx.App()
+    Interface(None, title='Calculator')
+
+    app.MainLoop()
 
